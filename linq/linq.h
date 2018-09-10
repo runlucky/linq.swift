fdf58f29cc21e918135ad1b3f15dc3decba0e77b
Kakeru Fukuda <stand.by.we@gmail.com>
2018-09-10 13:39:53 +0900

    ガワ作成

---
+++ linq/linq.h
@@ -0,0 +1,19 @@
+//
+//  linq.h
+//  linq
+//
+//  Created by H5266 on 2018/09/10.
+//  Copyright © 2018年 Kakeru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for linq.
+FOUNDATION_EXPORT double linqVersionNumber;
+
+//! Project version string for linq.
+FOUNDATION_EXPORT const unsigned char linqVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <linq/PublicHeader.h>
+
+
---
+++ linq/linq.swift
@@ -0,0 +1,13 @@
+//
+//  linq.swift
+//  linq
+//
+//  Created by H5266 on 2018/09/10.
+//  Copyright © 2018年 Kakeru. All rights reserved.
+//
+
+import Foundation
+
+public class linq {
+    public func hello() -> String { return "world" }
+}
