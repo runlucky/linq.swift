0d951d35c5e99a744dbe3873f533fd5c6e24a8d8
Kakeru Fukuda <stand.by.we@gmail.com>
2018-09-10 14:23:21 +0900

    countを作る

--- linq/linq.swift
+++ linq/linq.swift
@@ -11,3 +11,18 @@ import Foundation
 public class linq {
     public func hello() -> String { return "world" }
 }
+
+
+extension Sequence {
+    public func count(_ match: (Element) -> Bool) -> Int {
+        return reduce(0) { n, element in match(element) ? n + 1 : n }
+    }
+}
+
+extension Sequence where  Element: Equatable {
+    public func count(_ element: Element) -> Int {
+        return count { $0 == element }
+    }
+}
+
+
---
+++ linqTests/testCount.swift
@@ -0,0 +1,25 @@
+//
+//  testCount.swift
+//  linqTests
+//
+//  Created by H5266 on 2018/09/10.
+//  Copyright © 2018年 Kakeru. All rights reserved.
+//
+
+import XCTest
+@testable import linq
+
+class testCount: XCTestCase {
+    private let list = [3, 4, 2, 3, 5]
+
+    func testCountup() {
+        XCTAssertEqual(list.count, 5)
+        XCTAssertEqual(list.count { $0 < 4 }, 3)
+        XCTAssertEqual(list.count(3), 2)
+    }
+
+    func testEquatable() {
+        XCTAssertEqual(list.count(3), 2)
+        XCTAssertEqual(["a", "b", "c"].count("b"), 1)
+    }
+}
